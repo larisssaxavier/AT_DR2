5677d4c109ebcd4caf14f8b31ef41b437efd0886
larisssa <larissa.xaraujo@al.infnet.edu.br>
2025-04-03 21:49:19 -0300

    atualização

---
+++ Exercicio7/ContaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class ContaBancaria
+{
+	public string Titular { get; set; }
+	private decimal saldo;
+
+    public ContaBancaria(string titular, decimal saldoInicial = 0)
+	{
+        Titular = titular;
+        saldo = saldoInicial;
+    }
+    public void Depositar(decimal valor)
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine("\nValor de depósito deve ser maior que zero.");
+            Console.WriteLine($"Tentativa de depósito no valor de: R${valor}");
+            return;
+        }
+        saldo += valor;
+        Console.WriteLine($"Depósito de R${valor:F2} realizado com sucesso.");
+    }
+    public void Sacar(decimal valor)
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine("\nValor de saque deve ser maior que zero.");
+            return;
+        }
+        if (valor > saldo)
+        {
+            Console.WriteLine("\nSaldo insuficiente para realizar o saque.");
+            Console.WriteLine($"Valor de saque R${valor:F2} maior que o saldo atual.");
+            return;
+        }
+        saldo -= valor;
+        Console.WriteLine($"\nSaque de {valor:F2} realizado com sucesso.");
+    }
+    public void ExibirSaldo()
+    {
+        Console.WriteLine($"Saldo atual: R${saldo:F2}");
+    }
+}
---
+++ Exercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria contaLarissa = new ContaBancaria("Larissa", 25);
+        Console.WriteLine("Bem-vindo ao sistema bancário!\n");
+        Console.WriteLine($"Titular: {contaLarissa.Titular}\n");
+
+        contaLarissa.Depositar(256.56m);
+        contaLarissa.ExibirSaldo();
+
+        contaLarissa.Sacar(340.56m);
+        contaLarissa.ExibirSaldo();
+
+        contaLarissa.Sacar(100.50m);
+        contaLarissa.ExibirSaldo();
+
+        contaLarissa.Depositar(-100.50m);
+        contaLarissa.ExibirSaldo();
+
+    }
+}
